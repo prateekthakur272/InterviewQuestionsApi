2b914f842af63aa2c8a2d1222ca5bb42cf17318c
Prateek Thakur <prateekthakur272@gmail.com>
2023-08-27 01:27:36 +0530

    adds authorization

--- InterviewQuestionsApi/views.py
+++ InterviewQuestionsApi/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from questions.serializers import QuestionSerializer
+from questions.models import Question
+from rest_framework.permissions import IsAuthenticated
 
 
 class TestView(APIView):
+
+    permission_classes = (IsAuthenticated,)
+
     def get(self, req, *args, **kwargs):
-        data = {
-            'username': 'prateekthakur',
-            'active_since': 2018
-        }
-        return Response(data)
+        query = Question.objects.all()
+        serializer = QuestionSerializer(query, many=True)
+        return Response(serializer.data)
+
+    def post(self, req, *args, **kwargs):
+        serializer = QuestionSerializer(data=req.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return serializer.errors
